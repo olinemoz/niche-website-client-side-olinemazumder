48c5be5061fba00b39328b3b4c4f3ab386378b30
olinemazumder <olinemozumder@gmail.com>
2021-11-15 16:45:37 +0600

    banner fixes

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -5,7 +5,7 @@ const Banner = () => {
         <div className="p-4 shadow-lg" style={{marginTop: "60px", marginBottom: "20px"}}>
             <h3 className="text-center bg-primary py-3 rounded text-white">Best Car Shop Company in Bangladesh</h3>
             <img className="d-block w-75 mx-auto mt-3"
-                 src="https://thumbs.dreamstime.com/b/auto-car-repair-shop-garage-workshop-cars-56409562.jpg" alt=""/>
+                 src="https://i.ibb.co/HzXvnrV/banner.jpg" alt=""/>
         </div>
     );
 };
